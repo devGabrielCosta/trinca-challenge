f409e1e86b7402d63cbec696bae34c9975a9f450
devgabrielcosta <devgabrielcosta@gmail.com>
2023-12-06 01:34:41 -0300

    Implementado exibição da lista de compras

---
+++ Serverless-Api/Functions/BuyList/GetBuyList/GetBuyListRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serverless_Api.Functions.BuyList.GetBuyList
+{
+    public class GetBuyListRequest
+    {   
+        public string Id { get; set; }
+        public Dictionary<string, int> BuyList {  get; set; }
+    }
+}
